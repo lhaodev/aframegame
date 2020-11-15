7308d3224afa4b08e99d57b6b2cfbbd316e100a3
Glitch (aframe-teleport-controls-extra) <none>
2020-11-15 18:41:33 +0000

    💴⛑ Checkpoint ./follow.js:18374370/1196 ./L22a.html:18374370/62

--- follow.js
+++ follow.js
@@ -9,28 +9,32 @@ AFRAME.registerComponent('follow', {
 
   init: function () {
     this.directionVec3 = new THREE.Vector3();
+    this.counter=0
   },
 
   tick: function (time, timeDelta) {
     var directionVec3 = this.directionVec3;
+    this.counter += 1
+    this.show = this.counter%100 == 0
+    if (!this.show){
+      return
+    }
 
     // Grab position vectors (THREE.Vector3) from the entities' three.js objects.
     var targetPosition = this.data.target.object3D.position;
     var currentPosition = this.el.object3D.position;
-    console.log('target='+JSON.stringify(targetPosition))
-    console.log('current='+JSON.stringify(currentPosition))
+    
     window.avatar = this
 
     // Subtract the vectors to get the direction the entity should head in.
     directionVec3.copy(targetPosition).sub(currentPosition);
-    console.log('dirvec1='+JSON.stringify(directionVec3))
+    
     // Calculate the distance.
     var distance = directionVec3.length();
 
     // Scale the direction vector's magnitude down to match the speed.
     var factor = this.data.speed / distance;
-    console.log('distance='+distance)
-    console.log('factor='+factor)
+   
     window.dv = directionVec3
     directionVec3['x'] *= factor //* (timeDelta/1000)
     directionVec3['y'] *= factor //* (timeDelta/1000)
@@ -38,13 +42,22 @@ AFRAME.registerComponent('follow', {
     //['x', 'y', 'z'].forEach(function (axis) {
     //  directionVec3[axis] *= factor * (timeDelta / 1000);
     //});
-    console.log('dirvec2='+JSON.stringify(directionVec3))
-    console.log('\n\n')
-
+    
+    if (this.show) {
+      console.log('target='+JSON.stringify(targetPosition))
+      console.log('current='+JSON.stringify(currentPosition))
+      console.log('dirvec1='+JSON.stringify(directionVec3))
+      console.log('distance='+distance)
+      console.log('factor='+factor)
+      console.log('dirvec2='+JSON.stringify(directionVec3))
+      console.log('time='+time)
+      console.log('timeDelta='+timeDelta)
+      console.log('\n\n')
+    }
     // here we applhy a push toward the target
     if (this.el.body) {
       //console.log(distance)
-      console.log('pos='+this.el.object3D.position)
+      //console.log('pos='+this.el.object3D.position)
       this.el.body.applyImpulse(
          new CANNON.Vec3().copy(directionVec3),
          new CANNON.Vec3().copy(this.el.body.position)//getComputedAttribute('position'))
